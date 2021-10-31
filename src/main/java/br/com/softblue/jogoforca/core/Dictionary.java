02c921ff2e9cf9616e6073040450744203c8eb81
LuanFRSanches <luanfelipersanches@gmail.com>
2021-10-31 14:01:45 -0400

    Criando a classe Dictionary

--- src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -1,4 +1,31 @@
 package br.com.softblue.jogoforca.core;
 
+import br.com.softblue.jogoforca.game.GameException;
+import br.com.softblue.jogoforca.util.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Dictionary {
+
+    private static final String FILE_NAME = "dicionario.txt";
+    private final List<String> words = new ArrayList<>();
+
+    private void load() {
+        try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine();
+
+            }
+            if (words.size() == 0) {
+                throw new GameException("A lista de palavras não pode ser vazia");
+            }
+        }
+    }
+
+    public Word netWord() {
+        int pos = RandomUtils.newRandomNumber(0, words.size());
+        return new Word(words.get(pos));
+    }
 }
