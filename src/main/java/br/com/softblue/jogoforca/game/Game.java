2044f91ac7b4407a5a216810638854c76f61a7d4
LuanFRSanches <luanfelipersanches@gmail.com>
2021-11-01 12:41:18 -0400

    Implementando o loop do jogo

--- src/main/java/br/com/softblue/jogoforca/game/Game.java
+++ src/main/java/br/com/softblue/jogoforca/game/Game.java
@@ -1,14 +1,77 @@
 package br.com.softblue.jogoforca.game;
 
 import br.com.softblue.jogoforca.core.Dictionary;
+import br.com.softblue.jogoforca.core.InvalidCharacterException;
 import br.com.softblue.jogoforca.core.Word;
+import br.com.softblue.jogoforca.ui.UI;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Game {
-    public void start() {
-        Dictionary dictionary = Dictionary.getInstance();
-        System.out.println(dictionary);
+    private static final int MAX_ERRORS = 5;
 
+    public void start() {
+
+
+        Set<Character> useChars = new HashSet<>();
+        int errorCount = 0;
+
+        UI.print("Bem vindo ao jogo da Forca!");
+
+        Dictionary dictionary = Dictionary.getInstance();
         Word word = dictionary.netWord();
-        System.out.println(word.getOriginalWord());
+
+        UI.print("A palavra tem " + word.size() + " letras");
+
+
+        while (true) {
+            UI.print(word);
+            UI.printNewLine();
+
+            char c;
+            try {
+                c = UI.readChar("Digite uma letra: ");
+                if (useChars.contains(c)) {
+                    throw new InvalidCharacterException("Esta letra já foi utilizada");
+                }
+                useChars.add(c);
+
+                if (word.hasChar(c)) {
+
+                    UI.print("Você acertou uma letra!");
+
+                } else {
+
+                    errorCount++;
+                    if (errorCount < MAX_ERRORS) {
+                        UI.print("Você errou! você ainda pode errar " + (MAX_ERRORS - errorCount) + " vez(es)");
+
+                    }
+                }
+                UI.printNewLine();
+
+                if (word.discovered()) {
+
+                    UI.print("PARABÉNS! Você acertou a palavra correta: " + word.getOriginalWord());
+                    UI.print("Fim do jogo!");
+                    break;
+
+                }
+
+                if (errorCount == MAX_ERRORS) {
+
+                    UI.print("Você perdeu o jogo! A palavra correta era: " + word.getOriginalWord());
+                    UI.print("Fim do jogo!");
+                    break;
+                }
+
+
+            } catch (InvalidCharacterException e) {
+                UI.print("Erro: " + e.getMessage());
+                UI.printNewLine();
+            }
+        }
     }
+
 }
